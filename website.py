de965a0bcc63feea986ec9644dfbef9ad901050c
David Gonzalez <dagonz0421@gmail.com>
2024-10-19 11:36:33 -0500

    Adding a flask

--- website.py
+++ website.py
@@ -4,8 +4,19 @@ website = Flask(__name__)
 
 budgets = {}
 spending = {}
+credit_scores = {}
+savings_goals = {}
 
 @app.route('/')
 def home():
-    return render_template('.html')
+    return render_template('index.html')
+
+def setBudget():
+
+
+def Spending():
+
+def 
+
+
 
