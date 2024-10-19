068f17d3a4ce8649eb0cdde3bd41351287eaa672
Ces-r <trilotracker@gmail.com>
2024-10-19 15:39:52 -0500

    db stuff

---
+++ testapp.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+def db_connect():
+    db = sqlite3.connect("expenses.sqlite")
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.route('/budget/<username>')
+def home(username):
+    conn = db_connect()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM people')
+    people = cur.fetchall()
+    conn.close()
+    
+    return render_template('budget.html', people=people, username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# @app.route('/budget', methods=['POST'])
+# def budget():
+#     user = request.form['user']
+
+# @app.route('/savings', methods=['POST'])
+# def saving():
+#     user = request.form['user']
+
+# @app.route('/credit', methods=['POST'])
+# def credit():
+#     user = request.form['user']
+#     credit_scores = int(request.form['credit_score'])
+#     credit_scores[user] = credit_scores
+
+if __name__ == '__main__':
+    app.run(debug=True)
