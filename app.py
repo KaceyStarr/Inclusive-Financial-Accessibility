a3992fbc3697974614a4017b09bca168e9e485fe
NovaFirestar <hacknetworkserver@gmail.com>
2024-10-19 13:26:48 -0500

    static

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ savings_goals = {}
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('budget.html', budget = 10)
 
-@app.route('/budget', methods=['POST'])
-def budget():
-    user = request.form['user']
+# @app.route('/budget', methods=['POST'])
+# def budget():
+#     user = request.form['user']
 
-@app.route('/savings', methods=['POST'])
-def saving():
-    user = request.form['user']
+# @app.route('/savings', methods=['POST'])
+# def saving():
+#     user = request.form['user']
 
-@app.route('/credit', methods=['POST'])
-def credit():
-    user = request.form['user']
-    credit_scores = int(request.form['credit_score'])
-    credit_scores[user] = credit_scores
+# @app.route('/credit', methods=['POST'])
+# def credit():
+#     user = request.form['user']
+#     credit_scores = int(request.form['credit_score'])
+#     credit_scores[user] = credit_scores
 
 if __name__ == '__main__':
     app.run(debug=True)
